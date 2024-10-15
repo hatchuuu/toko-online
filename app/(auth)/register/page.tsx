ce3bf20221901545bf368654767f631f7ac7f73d
KHUSUMA <KHUSUMA@DANDELIONSPC>
2024-10-15 23:10:34 +0700

    NAYR : Init

---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+  <div className="p-6 space-y-4">
+    <h1 className='font-bold text-2xl text-gray-900'>Create an Account</h1>
+  </div>
+    )
+}
+
+export default Register
